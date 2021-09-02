c07ec1a77445a36fcfd324648fb2e0d87cd5c144
Matt Bush <59295261+matthewbush55@users.noreply.github.com>
2021-09-02 10:35:10 -0600

    Modified server.js to accept questions

--- server.js
+++ server.js
@@ -1,15 +1,11 @@
 // import required libraries
-const express = require("express");
+const inquirer = require("inquirer");
+const questions = require("./lib/questions");
 
-// boilerplate for middleware handling
-const app = express();
-const PORT = process.env.PORT || 3001;
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+//testing
+const init = async () => {
+  const selection = await inquirer.prompt(questions.mainMenu);
+  console.log(selection.mainMenu);
+};
 
-// might need to include routes here
-
-// start express listening port
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+init();
