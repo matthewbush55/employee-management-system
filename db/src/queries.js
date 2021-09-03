387d8661e138bad2684677f292cef4f2ba542fcb
Matt Bush <59295261+matthewbush55@users.noreply.github.com>
2021-09-02 19:43:12 -0600

    Added questions

--- db/src/queries.js
+++ db/src/queries.js
@@ -4,10 +4,19 @@ const cTable = require("console.table");
 
 // class holding all db query methods for use throughout the app
 class DBQuery {
-  constructor() {}
   viewEmployees() {
     const sqlQuery =
       'SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary,CONCAT (m.first_name," ", m.last_name) AS manager FROM employee LEFT JOIN employee as m ON m.id = employee.manager_id JOIN role ON employee.role_id = role.id JOIN department ON role.department_id = department.id';
+    dbConnection.query(sqlQuery, (err, results) => {
+      if (err) {
+        console.log(err);
+      } else {
+        return console.table(results);
+      }
+    });
+  }
+  addEmployeeToDB(data) {
+    const sqlQuery = ("INSERT INTO employee SET ?", data);
     dbConnection.query(sqlQuery, (err, results) => {
       if (err) {
         console.log(err);
--- server.js
+++ server.js
@@ -2,18 +2,50 @@
 const inquirer = require("inquirer");
 const questions = require("./lib/questions");
 const DBQuery = require("./db/src/queries");
-const { mainMenu } = require("./lib/questions");
+// banner libraries
+const banner = require("asciiart-logo");
+const config = require("./package.json");
 
-// set variable to user for new db queries
+// set variable creating an instance of the DBQuery class for new db queries
 const query = new DBQuery();
+// render banner image
+function renderBanner() {
+  console.log(banner(config).render());
+}
 
+// start the app and present user with selection options
 async function init() {
   const selection = await inquirer.prompt(questions.mainMenu);
   switch (selection.action) {
-    case mainMenu.choices[0]:
+    case questions.mainMenu.choices[0]:
       query.viewEmployees();
       break;
+    //TODO: write rest of switch cases below
+    case questions.mainMenu.choices[1]:
+      query.viewEmployees();
+      break;
+    case questions.mainMenu.choices[2]:
+      query.viewEmployees();
+      break;
+    case questions.mainMenu.choices[3]:
+      query.viewEmployees();
+      break;
+    case questions.mainMenu.choices[4]:
+      query.viewEmployees();
+      break;
+    case questions.mainMenu.choices[5]:
+      query.viewEmployees();
+      break;
+    case questions.mainMenu.choices[6]:
+      query.viewEmployees();
+      break;
+    case questions.mainMenu.choices[7]:
+      break;
+    default:
+      text = "Please select a valid choice";
   }
 }
 
+// main function calls
+renderBanner();
 init();
